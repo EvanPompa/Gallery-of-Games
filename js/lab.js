f10ebb86720c37fe3920e7e587d605b886da3791
danielfig1 <daanfigu@ucsc.edu>
2022-05-22 23:12:48 -0700

    Kirby and Doom
    Added Kirby and Doom to index.html and js.

--- js/lab.js
+++ js/lab.js
@@ -1,14 +1,17 @@
 //When adding a slideshow, please add another "1" to the slide index. so for ex: "[1,1,{insert 1 here}]"
-let slideIndex = [1,1,1,1];
+let slideIndex = [1,1,1,1,1,1];
 
 // When adding a slideshow, please add the name of the slides to this list.
-let slideId = ["mario-slides", "sonic-slides", "pokemon-slides", "tetris-slides", ];
+let slideId = ["mario-slides", "sonic-slides", "pokemon-slides", "tetris-slides", "kirby-slides", "doom-slides"];
 
 //When adding a slideshow, please add another function call and increment the second value ex: showSlides(1, {increment this number});
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+
 
 //this function cycles between slides
 function plusSlides(n, no) {
